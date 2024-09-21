78c2c81d79699710fe428e518fc16dcfa38ba5ba
yang <diddnjswns20@gmail.com>
2024-09-21 16:46:19 +0900

    SCaution css 구조 변경

--- src/components/SCaution.tsx
+++ src/components/SCaution.tsx
@@ -7,6 +7,7 @@ export interface SCautionProps {
   liStyle?: string;
   label?: string;
   contents: string[];
+  className?: string;
 }
 
 const SCaution = ({
@@ -15,30 +16,33 @@ const SCaution = ({
   useNotice = false,
   liStyle = '-',
   label,
-  contents = []
+  contents = [],
+  className
 } : SCautionProps ) => {
 
-  return (
-    <div
-      className={`flex rounded-md p-0 m-4 overflow-hidden text-Grey_Darken-4
-      ${useModal
-          ? useNotice ? 'bg-Grey_Lighten-6' : 'bg-Red_Lighten-6'
-          : 'bg-Red_Lighten-6 border border-Red_Lighten-3'
-      }`} >
+  const defaultSCatuion = `flex rounded-md p-0 m-4 overflow-hidden text-Grey_Darken-4`
+  const defaultIcon = `flex flex-col justify-center items-center min-w-104pxr max-w-104pxr p-6 bg-Red_Lighten-1`
+  const defaultLabel = `text-white block mt-2 font-bold text-16pxr`
+  const modal = useModal 
+                  ? useNotice ? 'bg-Grey_Lighten-6' : 'bg-Red_Lighten-6' 
+                  : 'bg-Red_Lighten-6 border border-Red_Lighten-3'
 
-      {!noIcon && (
-        <div className="flex flex-col justify-center items-center min-w-104pxr max-w-104pxr p-6 bg-Red_Lighten-1">
+  return (
+    <div className={[defaultSCatuion, modal, className].join(' ')}>
+
+      {noIcon || (
+        <div className={[defaultIcon].join(' ')}>
           <CautionIcon12 color="white"></CautionIcon12>
-          <strong className="text-white block mt-2 font-bold text-16pxr">{label}</strong>
+          <strong className={[defaultLabel].join(' ')}>{label}</strong>
         </div>
       )}
 
       <div className="p-5 ml-6">
         <ul className="list-none space-y-2">
-          {contents.map((item, index) => (
-            <li key={index}>
+          {contents.map((content, i) => (
+            <li key={i}>
               <span className="mr-3">{liStyle}</span>
-              {item}
+              {content}
             </li>
           ))}
         </ul>
