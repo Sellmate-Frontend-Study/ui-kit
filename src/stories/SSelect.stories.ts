f9266d3cefe1d3b05432392f9eaac13b6bbe99b9
진영진 <jin@seiim.co.kr>
2024-11-24 17:58:34 +0900

    s-select 스토리북추가

--- src/stories/SSelect.stories.ts
+++ src/stories/SSelect.stories.ts
@@ -20,18 +20,44 @@ type Story = StoryObj<typeof meta>;
 const options = [
 	{ label: 'option 1', value: 1 },
 	{ label: 'option 2', value: 2 },
+	{ label: 'option 3', value: 3 },
+	{ label: 'option 4', value: 4 },
+	{ label: 'option 5', value: 5 },
+	{ label: 'option 6', value: 6 },
+	{ label: 'option 7', value: 7 },
+	{ label: 'option 8', value: 8 },
+	{ label: 'option 9', value: 9 },
+	{ label: 'option 10', value: 10 },
+	{ label: 'option 11', value: 11 },
+	{ label: 'option 12', value: 12 },
 ];
 export const DefaultSelect: Story = {
 	args: {
-		classname: 'w-100pxr',
+		classname: 'w-200pxr',
 		options,
 	},
 };
 
 export const DisabledSelect: Story = {
 	args: {
-		classname: 'w-100pxr',
+		classname: 'w-200pxr',
 		options,
 		disabled: true,
+	},
+};
+
+export const MultipleSelect: Story = {
+	args: {
+		classname: 'w-200pxr',
+		options,
+		useMultiple: true,
+	},
+};
+
+export const CheckboxSelect: Story = {
+	args: {
+		classname: 'w-200pxr',
+		options,
+		useCheck: true,
 	},
 };
